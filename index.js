36056ee33ee91118c3b4efd5051c5e07e429d949
felipedelatorre <felipede_latorre@hotmail.com>
2019-05-20 22:06:32 -0700

    complete aritmethic

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 const greet = require('./lib/greet.js');
 const math = require('./lib/arithmetic.js');
-// console.log(greet('JOHN'));
-console.log(math.add([1,3,5,8])); // 17
-console.log(math.add([1,3,5,'8 '])); // 17
+console.log(greet('JOHN'));
+// console.log(math.add([1,3,5,8])); // 17
+// console.log(math.add([1,3,5,'hi'])); // 17
 
-console.log(math.subtract([100,10,50,20])); // 20
-console.log(math.subtract([100,10,50,'hi'])); // 20
+// console.log(math.subtract([100,10,50,20])); // 20
+// console.log(math.subtract([100,10,50,'hi'])); // 20
 
-console.log(math.multiply([2,2,2,2,2])); // 8
-// console.log(math.divide(8,0)); // 4
+// console.log(math.multiply([2,2,2,2,2])); // 8
+// console.log(math.divide(8,2)); // 4
