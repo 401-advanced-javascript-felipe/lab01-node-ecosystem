5c6f2a253b349d6d687cd30f3d5022d9f3a5f7f0
felipedelatorre <felipede_latorre@hotmail.com>
2019-05-21 22:34:43 -0700

    adding necessary packages

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 
 const greet = require('./lib/greet.js');
 const math = require('./lib/arithmetic.js');
+
+
+const faker = require('./node_modules/faker');
+
+console.log(faker.random.word());
+console.log(faker.random.arrayElement());
+
 console.log(greet('JOHN'));
-// console.log(math.add([1,3,5,8])); // 17
-// console.log(math.add([1,3,5,'hi'])); // 17
+console.log(math.add([1,3,5,8])); // 17
+console.log(math.add([1,3,5,'hi'])); // 17
 
-// console.log(math.subtract([100,10,50,20])); // 20
-// console.log(math.subtract([100,10,50,'hi'])); // 20
+console.log(math.subtract([100,10,50,20])); // 20
+console.log(math.subtract([100,10,50,'hi'])); // 20
 
-// console.log(math.multiply([2,2,2,2,2])); // 8
-// console.log(math.divide(8,2)); // 4
+console.log(math.multiply([2,2,2,2,2])); // 8
+console.log(math.divide(8,2)); // 4
